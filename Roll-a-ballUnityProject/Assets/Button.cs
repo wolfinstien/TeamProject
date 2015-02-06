421f037aa2244eb78d3b5e0895555baf495d18e9
BrandonWhite <bdon.brandon.white@gmail.com>
2015-02-06 20:58:32 +0000

    Added button to ExampleLevel, started a script to open the doors (part of the wall prefab) it doesnt work yet though.

---
+++ Roll-a-ballUnityProject/Assets/Button.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button : MonoBehaviour 
+{
+	public GameObject door;
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	// Should probably use fixed update here as it is for physics/aimation
+	void Update () 
+	{
+
+	}
+
+	void OnCollisionEnter()
+	{
+		Debug.Log ("button pressed");
+
+		for (int i=0; i<4; i++) 
+		{
+			door = GameObject.Find("Room/Walls&Floor/Wall/Door");
+			Debug.Log(GameObject.Find ("Room/Walls&Floor/Wall/Door").transform.position);
+			Debug.Log(door.transform.position);
+			Vector3 closed = door.transform.position;
+			//open door
+			closed.y += 3;
+			door.transform.position.Set (closed.x, closed.y, closed.z);
+		}
+	}
+}
