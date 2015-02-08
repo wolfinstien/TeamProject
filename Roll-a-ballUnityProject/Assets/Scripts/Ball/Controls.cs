26b77c8c569501bc3dfa49476acb6b14459a80e4
BrandonWhite <bdon.brandon.white@gmail.com>
2015-02-08 14:23:52 +0000

    Made the audio come from the ball so the collision part of the script can be used to trigger vibration too. the ball also has drag on it so won't roll wildly until it hits something. Also, cant remember if i said but there are now doors embedded in the walls and a button.

--- Roll-a-ballUnityProject/Assets/Scripts/Ball/Controls.cs
+++ Roll-a-ballUnityProject/Assets/Scripts/Ball/Controls.cs
@@ -7,7 +7,7 @@ public class Controls : MonoBehaviour
 	// Use this for initialization
 	void Start () 
 	{
-		Physics.gravity.Set(0.0f,-1000f,0.0f);
+		//Physics.gravity.Set(0.0f,-1000f,0.0f);
 	}
 	
 	// Update is called once per frame
@@ -53,10 +53,34 @@ public class Controls : MonoBehaviour
         #endif
 	}
 
-	void OnCollisionEnter()
+	void OnCollisionEnter(Collision other)
 	{
-		#if UNITY_ANDROID
-		Handheld.Vibrate ();
-		#endif
+		// todo: there is a problem with the relative position where it is taking the 
+		//local y coordinate of the sphere to do the above/bellow calculation meaning 
+		//sometimes it triggers above and sometimes not depending on which way up the sphere is.
+
+		Vector3 contactPoint = other.contacts[0].point;
+		var relativePosition = transform.InverseTransformPoint(contactPoint);
+		Debug.Log ("CP= " + contactPoint + " RP= " + relativePosition);
+
+		// dont want to colide with objects we are rollling on
+		if (!(relativePosition.y > 0))
+		{
+			Debug.Log("The object is not above.");
+
+			// these shouldnt trigger collisions either
+			if (/*other.gameObject.name != "FloorTL" &&
+			    other.gameObject.name != "FloorBL" &&
+			    other.gameObject.name != "FloorTR" &&
+			    other.gameObject.name != "FloorBR" &&*/
+			    other.gameObject.name != "CubeSlope")
+			{
+				Debug.Log(other.gameObject.name);
+				this.audio.Play();
+				#if UNITY_ANDROID
+				Handheld.Vibrate ();
+				#endif
+			}
+		}
 	}
 }
