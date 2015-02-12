3831319ccb11c05f985b9b9430eba49c99d9755f
wolfinstien <wolfinstien@live.co.uk>
2015-02-12 13:45:46 +0000

    Merge branch 'master' of https://github.com/PixelKnot/TeamProject
    Conflicts:
    	Roll-a-ballUnityProject/Assembly-CSharp-vs.csproj
    	Roll-a-ballUnityProject/Assembly-CSharp.csproj
    	Roll-a-ballUnityProject/Assets/ExampleLevel.unity
    	Roll-a-ballUnityProject/Library/AssetServerCacheV3
    	Roll-a-ballUnityProject/Library/CurrentLayout.dwlt
    	Roll-a-ballUnityProject/Library/ScriptAssemblies/Assembly-CSharp.dll
    
    	Roll-a-ballUnityProject/Library/ScriptAssemblies/Assembly-CSharp.dll.mdb
    	Roll-a-ballUnityProject/Library/assetDatabase3
    	Roll-a-ballUnityProject/Library/expandedItems
    	Roll-a-ballUnityProject/Library/guidmapper
    	Roll-a-ballUnityProject/ProjectSettings/TagManager.asset
    	Roll-a-ballUnityProject/Roll-a-ballUnityProject-csharp.sln
    	Roll-a-ballUnityProject/Roll-a-ballUnityProject.sln
    	Roll-a-ballUnityProject/Roll-a-ballUnityProject.userprefs
    
    I have done the colour gates and colour channging powerups

---
+++ Roll-a-ballUnityProject/Assets/Scripts/Obstacles/ColourChange.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColourChange : MonoBehaviour {
+
+	public string colour;
+
+	// Use this for initialization
+	void Start () {
+		switch (colour)
+		{
+		case "Red":
+			this.renderer.material.color = Color.red;
+			break;
+
+		case "Blue":
+			this.renderer.material.color = Color.blue;
+			break;
+
+		case "Green":
+			this.renderer.material.color = Color.green;
+			break;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Roll-a-ballUnityProject/Assets/Scripts/Obstacles/PushBlock.cs
+++ Roll-a-ballUnityProject/Assets/Scripts/Obstacles/PushBlock.cs
@@ -4,7 +4,12 @@ using System.Collections;
 public class PushBlock : MonoBehaviour {
 
     #region Fields
-
+	//Added by adam
+	public float maxDistance;
+	public float minDistance;
+	private float frontLimit;
+	private float backLimit;
+	//
     private const float m_boundsNode_A = 5f, 
                         m_boundsNode_B = -2f;
 
@@ -20,20 +25,39 @@ public class PushBlock : MonoBehaviour {
 
     // Use this for initialization
 	void Start () {
-        this.rigidbody.isKinematic = true;
+        //this.rigidbody.isKinematic = true;
+		//added by adam
+		backLimit = this.transform.position.z + maxDistance;
+		frontLimit = this.transform.position.z - minDistance;
+		//
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	/*void Update () {
         GameObject ballRef = GameObject.FindGameObjectWithTag("Player");
         
         if ((this.transform.position.z >= m_boundsNode_A && ballRef.transform.position.z < m_boundsNode_A) ||
             (this.transform.position.z <= m_boundsNode_B && ballRef.transform.position.z > m_boundsNode_B)) {
             this.rigidbody.isKinematic = true;
         }
-    }
+    }*/
 
-	void OnCollisionEnter(Collision other) {
+	//added by adam
+	void FixedUpdate(Collision other)
+	{
+		float currentPos = this.transform.position.z;
+		if (currentPos < frontLimit && currentPos > backLimit)
+		{
+			this.rigidbody.isKinematic = false;
+		}
+		else
+		{
+			this.rigidbody.isKinematic = true;
+			Debug.Log ("Stop");
+		}
+	}
+
+	/*void OnCollisionEnter(Collision other) {
 		if (other.gameObject.tag.Equals("Player")) 
             this.rigidbody.isKinematic = false;
 		
@@ -47,7 +71,7 @@ public class PushBlock : MonoBehaviour {
 
     void OnCollisionExit(Collision other) {
         this.rigidbody.isKinematic = true;
-    }
+    }*/
 
     #endregion
 }
