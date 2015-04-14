910afa4227d55f7e323d85a10e0afcc282ab344d
BrandonWhite <bdon.brandon.white@gmail.com>
2015-04-14 16:18:20 +0100

    another room added as well as jump ramps

---
+++ Roll-a-ballUnityProject/Assets/RampForce.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RampForce : MonoBehaviour 
+{
+    public float magnitudeForce;
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Ramp!");
+        other.GetComponent<Rigidbody>().AddForce(this.transform.forward*-magnitudeForce);
+    }
+}
